99f3dff5b50dc06e2a3d5b2a8e5b898ddc9887b2
Adam Kahtava <adam@kahtava.com>
2023-11-15 11:47:37 -0500

    Swagger / Spec tests for Posts / Discussions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
   def index
     respond_with(Post.all)
   end
+
+  def create
+    post = params.require(:post).permit!
+    user = params.require(:user).permit!
+
+    @user = User.find(user[:id])
+    @post = Post.create(post.merge({ user: @user }))
+
+    respond_with(@post)
+  end
+
+  def show
+    respond_with(Post.find(params[:id]))
+  end
+
+  def comments
+    respond_with(Post.find(params[:id]).comments)
+  end
 end
---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,95 @@
+require 'swagger_helper'
+require 'faker'
+
+RSpec.describe 'posts', type: :request do
+
+  path '/posts' do
+
+    get('list posts') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create post') do
+      consumes 'application/json'
+      parameter name: :discussion, in: :body, required: true, schema: {
+        type: :object,
+        properties: {
+          title: { type: :string },
+          content: { type: :string },
+          user: {
+            properties: {
+              id: { type: :string }
+            }
+          }
+        },
+      }
+
+      response(201, 'successful') do
+        user = User.create({ first_name: Faker::Name.first_name, last_name: Faker::Name.last_name })
+        let(:discussion) { { title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user: { id: user.id } } }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/posts/{id}' do
+    parameter name: 'id', in: :path, type: :string
+
+    get('show post') do
+      response(200, 'successful') do
+        user = User.create({ first_name: Faker::Name.first_name, last_name: Faker::Name.last_name })
+        post = Post.create({ title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user: user })
+        let(:id) { post.id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/posts/{id}/comments' do
+    parameter name: 'id', in: :path, type: :string
+
+    get('show post comments') do
+      response(200, 'successful') do
+        user = User.create({ first_name: Faker::Name.first_name, last_name: Faker::Name.last_name })
+        post = Post.create({ title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user: user })
+        Comment.create({ post: post, content: Faker::Lorem.paragraph, user: user })
+        let(:id) { post.id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
