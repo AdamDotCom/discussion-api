152413210c703bee80f82fbc7a318d1a818fa20e
Adam Kahtava <adam@kahtava.com>
2023-11-15 10:43:02 -0500

    Swagger / Spec tests for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def index
     respond_with(User.all)
   end
+
+  def create
+    user = params.require(:user).permit!
+
+    @user = User.create(user)
+
+    respond_with(@user)
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -17,5 +17,29 @@ RSpec.describe 'users', type: :request do
         run_test!
       end
     end
+
+    post('create user') do
+      consumes 'application/json'
+      parameter name: :user, in: :body, required: true, schema: {
+        type: :object,
+        properties: {
+          first_name: { type: :string },
+          last_name: { type: :string }
+        },
+      }
+
+      response(201, 'successful') do
+        let(:user) { { first_name: 'first', last_name: 'last' } }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
