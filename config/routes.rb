a283482b65b09f52d1ecb7fe6f22c0503fa3578e
Adam Kahtava <adam@kahtava.com>
2023-11-21 13:47:49 -0500

    Clean up: redirect to Swagger docs on root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #
+  root to: redirect('/api-docs')
 
   resources :users, :defaults => { :format => 'json' } do
     resources :comments
